b139b57b9baa07ec014cfc175e992ec255c649b1
RefinedStone <chem.en.9273@gmail.com>
2022-12-03 04:51:46 +0900

    settings: ci-cd please made me sleep , #7

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,4 +19,4 @@ else
 fi
 
 echo "> $JAR_PATH 배포" #3
-nohup java -jar /home/ubuntu/app/deploy/SpeedSideProject-0.0.1-SNAPSHOT.jar &
+nohup java -jar /home/ubuntu/app/deploy/SpeedSideProject-0.0.1-SNAPSHOT.jar /dev/null 2> /dev/null < /dev/null &
