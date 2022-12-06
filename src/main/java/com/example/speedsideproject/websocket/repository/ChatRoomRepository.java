e4a4f93df7f53a62efa5eac4b5551b74ff120bc5
JooLegend <ehenr2003@naver.com>
2022-12-07 00:55:34 +0900

    Websocket 세팅 설정

---
+++ src/main/java/com/example/speedsideproject/websocket/repository/ChatRoomRepository.java
@@ -0,0 +1,36 @@
+package com.example.speedsideproject.websocket.repository;
+
+import com.example.speedsideproject.websocket.dto.ChatRoomDTO;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+
+@Repository
+public class ChatRoomRepository {
+    private Map<String, ChatRoomDTO> chatRoomDTOMap;
+
+    @PostConstruct
+    private void init(){
+        chatRoomDTOMap = new LinkedHashMap<>();
+    }
+
+    public List<ChatRoomDTO> findAllRooms(){
+        //채팅방 생성 순서 최근 순으로 반환
+        List<ChatRoomDTO> result = new ArrayList<>(chatRoomDTOMap.values());
+        Collections.reverse(result);
+
+        return result;
+    }
+
+    public ChatRoomDTO findRoomById(String id){
+        return chatRoomDTOMap.get(id);
+    }
+
+    public ChatRoomDTO createChatRoomDTO(String name){
+        ChatRoomDTO room = ChatRoomDTO.create(name);
+        chatRoomDTOMap.put(room.getRoomId(), room);
+
+        return room;
+    }
+}
