9d3d5bab9ed841c4c65bee50f96faa0c6ddfc820
RefinedStone <chem.en.9273@gmail.com>
2022-12-08 22:48:18 +0900

    fix: cors settings fix

--- src/main/java/com/example/speedsideproject/sse/NotificationController.java
+++ src/main/java/com/example/speedsideproject/sse/NotificationController.java
@@ -1,12 +1,7 @@
 package com.example.speedsideproject.sse;
 
-import com.example.speedsideproject.security.user.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequiredArgsConstructor
