30da91dd53250e97e069ca0562461efecaf2058e
JooLegend <ehenr2003@naver.com>
2022-12-13 22:18:12 +0900

    fix: stomp test

--- src/main/java/com/example/speedsideproject/websocket/dto/ChatRoomDTO.java
+++ src/main/java/com/example/speedsideproject/websocket/dto/ChatRoomDTO.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 public class ChatRoomDTO {
     private String roomId;
     private String name;
+
     private Set<WebSocketSession> sessions = new HashSet<>();
 
     //WebSocketSession은 Spring에서 Websocket Connection이 맺어진 세션
