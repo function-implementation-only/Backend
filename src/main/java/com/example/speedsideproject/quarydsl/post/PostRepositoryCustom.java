bdf8eb0b8a15214dc5046295a96a7cd61fa2b27b
RefinedStone <chem.en.9273@gmail.com>
2022-12-29 08:21:58 +0900

    fix: post entity 코드 갈무리, #22

---
+++ src/main/java/com/example/speedsideproject/quarydsl/post/PostRepositoryCustom.java
@@ -0,0 +1,26 @@
+package com.example.speedsideproject.quarydsl.post;
+
+
+import com.example.speedsideproject.post.PostResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface PostRepositoryCustom {
+
+    //mypage에서 내가 like 한 postList  불러오기
+//    List<Post> findAllMyLikes(Member member);
+
+    //post get all
+    Page<PostResponseDto> findAllMyPost(Pageable pageable);
+    // post get all - search
+    Page<PostResponseDto> findAllMyPostWithSearch(Pageable pageable, String string);
+
+
+
+    //post - category 정렬 + 내가 한 좋아요
+    Page<?> findAllPostWithCategory(Pageable pageable, String category);
+
+    //post -category - search
+    Page<?> findAllPostWithCategoryWithSearch(Pageable pageable, String category, String search);
+
+}
