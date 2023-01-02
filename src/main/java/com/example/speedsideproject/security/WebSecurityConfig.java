abf0756f5127f63a992b87ece2b41328f282af50
JooLegend <ehenr2003@naver.com>
2023-01-02 23:13:02 +0900

    fix: websocket

--- src/main/java/com/example/speedsideproject/security/WebSecurityConfig.java
+++ src/main/java/com/example/speedsideproject/security/WebSecurityConfig.java
@@ -81,7 +81,7 @@ public class WebSecurityConfig {
                 .antMatchers("/posts/**").permitAll()
                 .antMatchers("/comment/**").permitAll()
                 .antMatchers("/create/**").permitAll()
-                .antMatchers("/chat/**").permitAll()
+                .antMatchers("/ws/chat/**").permitAll()
                 .antMatchers("/**").permitAll()
                 //swagger
                 .antMatchers(PERMIT_URL_ARRAY).permitAll()
