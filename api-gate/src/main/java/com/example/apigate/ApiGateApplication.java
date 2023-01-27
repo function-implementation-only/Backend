7e45c653f3c9510161ffda62de905968150af346
RefinedStone <chem.en.9273@gmail.com>
2023-01-28 01:15:11 +0900

    fix: api-gate에 jwt 검증 필터 추가

---
+++ api-gate/src/main/java/com/example/apigate/ApiGateApplication.java
@@ -0,0 +1,15 @@
+package com.example.apigate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateApplication.class, args);
+    }
+
+}
---
+++ api-gate/src/main/java/com/example/apigate/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,50 @@
+package com.example.apigate.filter;
+
+import com.example.apigate.jwt.util.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    private final JwtUtil jwtUtil;
+
+    @Autowired
+    public AuthorizationHeaderFilter(JwtUtil jwtUtil) {
+        super(Config.class);
+        this.jwtUtil = jwtUtil;
+    }
+
+    public static class Config {
+        //config
+    }
+
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            //ServerHttpRequest
+            ServerHttpRequest request = exchange.getRequest();
+
+            List<String> accessToken = jwtUtil.getHeaderToken(request, "Access");
+            List<String> refreshToken = jwtUtil.getHeaderToken(request, "Refresh");
+
+            if (accessToken==null) {
+                return jwtUtil.onError(exchange, "No Access token", HttpStatus.UNAUTHORIZED);
+            }
+            if (!jwtUtil.tokenValidation(accessToken.get(0))) {
+                return jwtUtil.onError(exchange, "AccessToken is not Valid", HttpStatus.UNAUTHORIZED);
+            }
+            return chain.filter(exchange);
+        };
+    }
+
+}
