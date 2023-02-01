f2971d401c347d6eeae29e62e4511ec8e30e5805
RefinedStone <chem.en.9273@gmail.com>
2023-02-02 06:10:49 +0900

    settings: MSA main-service setting to Eureka Discovery server, #46

--- src/main/java/com/example/speedsideproject/SpeedSideProject.java
+++ src/main/java/com/example/speedsideproject/SpeedSideProject.java
@@ -2,6 +2,7 @@ package com.example.speedsideproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
@@ -10,6 +11,7 @@ import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableEurekaClient
 public class SpeedSideProject {
 
     @PostConstruct
