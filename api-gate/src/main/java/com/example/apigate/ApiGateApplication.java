f07669fe3d88b84d5cab0c06d40178b7f5209eb1
RefinedStone <chem.en.9273@gmail.com>
2023-02-05 03:41:01 +0900

    feat: api-gateway에 spring cloud config 설정 추가, #2

--- api-gate/src/main/java/com/example/apigate/ApiGateApplication.java
+++ api-gate/src/main/java/com/example/apigate/ApiGateApplication.java
@@ -1,8 +1,11 @@
 package com.example.apigate;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -11,5 +14,11 @@ public class ApiGateApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGateApplication.class, args);
     }
+
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
+
 
 }
