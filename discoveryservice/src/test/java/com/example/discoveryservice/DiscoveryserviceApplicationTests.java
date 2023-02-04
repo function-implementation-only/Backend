abb6a7854159727da3732dc2ccd95adc2c530659
RefinedStone <chem.en.9273@gmail.com>
2023-02-05 02:33:22 +0900

    fix: config-service 저장 위치 변경, #3

--- discoveryservice/src/test/java/com/example/discoveryservice/DiscoveryserviceApplicationTests.java
+++ discoveryservice/src/test/java/com/example/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.discoveryservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DiscoveryserviceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.discoveryservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DiscoveryserviceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
