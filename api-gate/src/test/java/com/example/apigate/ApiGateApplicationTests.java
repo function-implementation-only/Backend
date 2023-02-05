38952596005a841d6168558e45d9fa6899b3f00f
RefinedStone <chem.en.9273@gmail.com>
2023-02-05 09:28:53 +0900

    feat: 모든 서비스를 docker화 하기 위한 준비

--- api-gate/src/test/java/com/example/apigate/ApiGateApplicationTests.java
+++ api-gate/src/test/java/com/example/apigate/ApiGateApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.apigate;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApiGateApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.apigate;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApiGateApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
