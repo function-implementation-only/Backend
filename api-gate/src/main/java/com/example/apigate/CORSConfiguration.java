0490790ab816f47200de3af113c7fe35402c45a8
RefinedStone <chem.en.9273@gmail.com>
2023-02-08 17:24:44 +0900

    feat: Spring cloud api-gate-way cors settings

---
+++ api-gate/src/main/java/com/example/apigate/CORSConfiguration.java
@@ -0,0 +1,41 @@
+package com.example.apigate;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CORSConfiguration implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOrigins("http://127.0.0.1:5173")
+                .allowedOrigins("https://www.joinus.store")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOriginPatterns("*")
+                .exposedHeaders(HttpHeaders.SET_COOKIE);
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+//        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedOrigin("http://127.0.0.1:5173");
+        corsConfiguration.addAllowedOrigin("https://www.joinus.store");
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addExposedHeader(HttpHeaders.SET_COOKIE);
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(corsConfigurationSource);
+    }
+}
---
+++ api-gate/src/main/java/com/example/apigate/TestController.java
@@ -0,0 +1,15 @@
+package com.example.apigate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+    @GetMapping("/test")
+    public String index() {
+        return "index";
+    }
+}
