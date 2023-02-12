a622fd84d1ab56fb6e53df19a0b0ae1aa24253bd
RefinedStone <chem.en.9273@gmail.com>
2023-02-12 18:05:46 +0900

    feat: feignClient settings + TestController 3 추가

--- src/main/java/com/example/chatservice/ChatServiceApplication.java
+++ src/main/java/com/example/chatservice/ChatServiceApplication.java
@@ -2,7 +2,9 @@ package com.example.chatservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ChatServiceApplication {
 
--- src/main/java/com/example/chatservice/tests/TestController.java
+++ src/main/java/com/example/chatservice/tests/TestController.java
@@ -1,12 +1,20 @@
 package com.example.chatservice.tests;
 
 import com.example.chatservice.config.security.user.UserDetailsImpl;
+import com.example.chatservice.feignclient.MainServiceClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    private MainServiceClient mainServiceClient;
+    @Autowired
+    public TestController(MainServiceClient mainServiceClient){
+        this.mainServiceClient = mainServiceClient;
+    }
     @GetMapping("/test")
     public String test() {
         return "test";
@@ -16,4 +24,10 @@ public class TestController {
     public String test2(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return userDetails.getAccount();
     }
+    @GetMapping("/test3")
+    public Object test3(@RequestHeader("auth") String auth, @RequestHeader("ACCOUNT-VALUE") String accountValue) {
+        Object myInfo = mainServiceClient.getInfo(auth,accountValue);
+        return myInfo;
+    }
+    
 }
