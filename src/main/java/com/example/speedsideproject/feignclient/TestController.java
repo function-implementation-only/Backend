dbbb4031a78b17cf4d68fd56044989556255b21b
RefinedStone <chem.en.9273@gmail.com>
2023-02-23 04:17:26 +0900

    fix: conflict source code, #48

--- src/main/java/com/example/speedsideproject/feignclient/TestController.java
+++ src/main/java/com/example/speedsideproject/feignclient/TestController.java
@@ -1,28 +1,33 @@
 package com.example.speedsideproject.feignclient;
 
+import com.example.speedsideproject.client.ChatServiceClient;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class TestController {
-    private NotificationServiceClient notificationServiceClient;
+    private ChatServiceClient chatServiceClient;
 
-    @Autowired
-    public TestController(NotificationServiceClient notificationServiceClient){
-        this.notificationServiceClient = notificationServiceClient;
-    }
 
     @GetMapping(value = "/test4", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     SseEmitter subscribe(@RequestHeader("auth") String auth,
         @RequestHeader("ACCOUNT-VALUE") String accountValue,
         @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId){
-        return notificationServiceClient.subscribe(auth, accountValue, lastEventId);
+        return chatServiceClient.subscribe(auth, accountValue, lastEventId);
+    }
+    @GetMapping("/test")
+    public String test(HttpServletRequest request, HttpServletResponse response) {
+
+        return "test";
     }
 }
